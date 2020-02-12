bde353ec12f5e618a599f028e7006570dfbd205e
Deim-Sha <deim.sha@gmail.com>
2020-02-12 22:08:26 +0300

    init project, add commander

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const program = require('commander');
+
+program
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.');
+
+program.parse(process.argv);
