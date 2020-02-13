2655c4f4c480419bfd5ea951400869c2e7ec2838
Deim-Sha <deim.sha@gmail.com>
2020-02-13 22:10:53 +0300

    add arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,12 @@ const require = createRequire(import.meta.url);
 const program = require('commander');
 
 program
-  .version('0.1.0')
-  .description('Compares two configuration files and shows a difference.');
+  .version('0.2.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(firstConfig, secondConfig);
+  });
 
 program.parse(process.argv);
