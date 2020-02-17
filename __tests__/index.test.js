e9f7062d9caceb49c6eb64ba1657ab6aa0956301
Deim-Sha <deim.sha@gmail.com>
2020-02-17 23:33:00 +0300

    test: refactor tests, add test for ini

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,16 +5,9 @@ import genDiff from '../src';
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
-test('gendiff json', () => {
-  const before = getFixturePath('before.json');
-  const after = getFixturePath('after.json');
-  const result = readFile('result.txt');
-  expect(genDiff(before, after)).toEqual(result);
-});
-
-test('gendiff yaml', () => {
-  const before = getFixturePath('before.yml');
-  const after = getFixturePath('after.yml');
+test.each(['json', 'yml', 'ini'])('gendiff %s', (ext) => {
+  const before = getFixturePath(`before.${ext}`);
+  const after = getFixturePath(`after.${ext}`);
   const result = readFile('result.txt');
   expect(genDiff(before, after)).toEqual(result);
 });
