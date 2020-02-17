6ad6327b591e75041e8b3bf7e6bc4cfae93c99ec
Deim-Sha <deim.sha@gmail.com>
2020-02-17 22:55:30 +0300

    refactor: add convertToObj function

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import path from 'path';
 import _ from 'lodash';
 import parse from './parsers';
 
-const genDiff = (pathToFile1, pathToFile2) => {
-  const fullPathToFile1 = path.resolve(process.cwd(), pathToFile1);
-  const fullPathToFile2 = path.resolve(process.cwd(), pathToFile2);
+const convertToObj = (filePath) => {
+  const filePathFull = path.resolve(process.cwd(), filePath);
+  const fileContent = fs.readFileSync(filePathFull, 'utf8');
+  const fileExt = path.extname(filePath);
+  return parse(fileContent, fileExt);
+};
 
-  const fileContent1 = fs.readFileSync(fullPathToFile1, 'utf8');
-  const fileContent2 = fs.readFileSync(fullPathToFile2, 'utf8');
-
-  const fileExt1 = path.extname(pathToFile1);
-  const fileExt2 = path.extname(pathToFile2);
-
-  const config1 = parse(fileContent1, fileExt1);
-  const config2 = parse(fileContent2, fileExt2);
+const genDiff = (filePath1, filePath2) => {
+  const config1 = convertToObj(filePath1);
+  const config2 = convertToObj(filePath2);
 
   const keys = _.union(Object.keys(config1), Object.keys(config2));
   const result = keys.reduce((acc, key) => {
