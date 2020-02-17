d82dbd05ed7ab60d97ed012ccf8320ff1025b575
Deim-Sha <deim.sha@gmail.com>
2020-02-17 21:52:59 +0300

    fix format

--- src/parsers.js
+++ src/parsers.js
@@ -2,7 +2,7 @@ import yaml from 'js-yaml';
 
 const parser = {
   '.json': (content) => JSON.parse(content),
-  '.yaml': (content) => yaml.safeLoad(content),
+  '.yml': (content) => yaml.safeLoad(content),
 };
 
 export default (content, ext) => parser[ext](content);
