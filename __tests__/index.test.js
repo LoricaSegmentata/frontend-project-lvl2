93b51e8ce87a7548e7f0307e771740f3e69d21d9
Deim-Sha <deim.sha@gmail.com>
2020-02-22 23:32:53 +0300

    test: add tests for plain format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,9 +5,17 @@ import genDiff from '../src';
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
-test.each(['json', 'yml', 'ini'])('gendiff %s', (ext) => {
+test.each([
+  ['json', 'complex'],
+  ['json', 'plain'],
+  ['yml', 'complex'],
+  ['yml', 'plain'],
+  ['ini', 'complex'],
+  ['ini', 'plain'],
+])('gendiff %s to %s format', (ext, format) => {
   const before = getFixturePath(`before.${ext}`);
   const after = getFixturePath(`after.${ext}`);
-  const result = readFile('result.txt');
-  expect(genDiff(before, after)).toEqual(result);
+  const result = readFile(format);
+
+  expect(genDiff(before, after, format)).toEqual(result);
 });
