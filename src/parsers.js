028ecbe9a88c7717d6a70d77049d81b08aa4963f
Deim-Sha <deim.sha@gmail.com>
2020-03-02 20:14:54 +0300

    refactor: make parse function pure

--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,5 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
-import fs from 'fs';
-import path from 'path';
 
 const parser = {
   '.json': JSON.parse,
@@ -9,9 +7,4 @@ const parser = {
   '.ini': ini.parse,
 };
 
-export default (filePath) => {
-  const fullPath = path.resolve(process.cwd(), filePath);
-  const content = fs.readFileSync(fullPath, 'utf8');
-  const ext = path.extname(filePath);
-  return parser[ext](content);
-};
+export default (ext, content) => parser[ext](content);
