cead5d5bb80628acce9fc9d54c9e2cb1e27165f9
Deim-Sha <deim.sha@gmail.com>
2020-03-07 18:50:10 +0300

    refactor: move rendering from renderDiff function

--- src/formatters/complex.js
+++ src/formatters/complex.js
@@ -13,18 +13,19 @@ const stringify = (item, depth) => {
   return `{\n${result}\n${getIndent(depth)}}`;
 };
 
+const rendering = {
+  deleted: (node, depth) => `${getIndent(depth - 1)}  - ${node.name}: ${stringify(node.value, depth)}`,
+  added: (node, depth) => `${getIndent(depth - 1)}  + ${node.name}: ${stringify(node.value, depth)}`,
+  unchanged: (node, depth) => `${getIndent(depth)}${node.name}: ${stringify(node.value, depth)}`,
+  changed: (node, depth) => [
+    `${getIndent(depth - 1)}  - ${node.name}: ${stringify(node.oldValue, depth)}`,
+    `${getIndent(depth - 1)}  + ${node.name}: ${stringify(node.newValue, depth)}`,
+  ],
+  parent: (node, depth, render) => `${getIndent(depth)}${node.name}: {\n${render(node.children, depth + 1)}\n${getIndent(depth)}}`,
+};
+
 const renderDiff = (tree, depth) => {
-  const rendering = {
-    deleted: (node) => `${getIndent(depth - 1)}  - ${node.name}: ${stringify(node.value, depth)}`,
-    added: (node) => `${getIndent(depth - 1)}  + ${node.name}: ${stringify(node.value, depth)}`,
-    unchanged: (node) => `${getIndent(depth)}${node.name}: ${stringify(node.value, depth)}`,
-    changed: (node) => [
-      `${getIndent(depth - 1)}  - ${node.name}: ${stringify(node.oldValue, depth)}`,
-      `${getIndent(depth - 1)}  + ${node.name}: ${stringify(node.newValue, depth)}`,
-    ],
-    parent: (node) => `${getIndent(depth)}${node.name}: {\n${renderDiff(node.children, depth + 1)}\n${getIndent(depth)}}`,
-  };
-  const result = tree.map((node) => rendering[node.status](node));
+  const result = tree.map((node) => rendering[node.status](node, depth, renderDiff));
 
   return _.flatten(result).join('\n');
 };
