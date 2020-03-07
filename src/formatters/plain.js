8a24d666f9afdad1769603718817f95bfaea9725
Deim-Sha <deim.sha@gmail.com>
2020-03-07 18:55:03 +0300

    refactor: move rendering from renderDiff function

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -11,16 +11,17 @@ const stringify = (item) => {
   return '[complex value]';
 };
 
+const rendering = {
+  deleted: (node, pathAcc) => `Property '${[...pathAcc, node.name].join('.')}' was deleted`,
+  added: (node, pathAcc) => `Property '${[...pathAcc, node.name].join('.')}' was added with value: ${stringify(node.value)}`,
+  changed: (node, pathAcc) => `Property '${[...pathAcc, node.name].join('.')}' was changed from ${stringify(node.oldValue)} to ${stringify(node.newValue)}`,
+  parent: (node, pathAcc, render) => render(node.children, [...pathAcc, node.name]),
+};
+
 const renderDiff = (tree, pathAcc) => {
-  const rendering = {
-    deleted: (node) => `Property '${[...pathAcc, node.name].join('.')}' was deleted`,
-    added: (node) => `Property '${[...pathAcc, node.name].join('.')}' was added with value: ${stringify(node.value)}`,
-    changed: (node) => `Property '${[...pathAcc, node.name].join('.')}' was changed from ${stringify(node.oldValue)} to ${stringify(node.newValue)}`,
-    parent: (node) => renderDiff(node.children, [...pathAcc, node.name]),
-  };
   const filtered = tree.filter((node) => node.status !== 'unchanged');
 
-  return filtered.map((node) => rendering[node.status](node)).join('\n');
+  return filtered.map((node) => rendering[node.status](node, pathAcc, renderDiff)).join('\n');
 };
 
 export default (tree) => renderDiff(tree, []);
