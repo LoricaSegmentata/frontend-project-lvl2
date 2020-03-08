39dd5db007e671151cb99f1a57b71c9a5393ad76
Deim-Sha <deim.sha@gmail.com>
2020-03-08 12:09:18 +0300

    fix naming

--- src/formatters/complex.js
+++ src/formatters/complex.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const getIndent = (depth) => '    '.repeat(depth);
+const makeIndent = (depth) => '    '.repeat(depth);
 
 const stringify = (item, depth) => {
   if (!_.isObject(item)) {
@@ -8,20 +8,20 @@ const stringify = (item, depth) => {
   }
 
   const keys = Object.keys(item);
-  const result = keys.map((key) => `${getIndent(depth + 1)}${key}: ${item[key]}`).join('\n');
+  const result = keys.map((key) => `${makeIndent(depth + 1)}${key}: ${item[key]}`).join('\n');
 
-  return `{\n${result}\n${getIndent(depth)}}`;
+  return `{\n${result}\n${makeIndent(depth)}}`;
 };
 
 const rendering = {
-  deleted: (node, depth) => `${getIndent(depth - 1)}  - ${node.name}: ${stringify(node.value, depth)}`,
-  added: (node, depth) => `${getIndent(depth - 1)}  + ${node.name}: ${stringify(node.value, depth)}`,
-  unchanged: (node, depth) => `${getIndent(depth)}${node.name}: ${stringify(node.value, depth)}`,
+  deleted: (node, depth) => `${makeIndent(depth - 1)}  - ${node.name}: ${stringify(node.value, depth)}`,
+  added: (node, depth) => `${makeIndent(depth - 1)}  + ${node.name}: ${stringify(node.value, depth)}`,
+  unchanged: (node, depth) => `${makeIndent(depth)}${node.name}: ${stringify(node.value, depth)}`,
   changed: (node, depth) => [
-    `${getIndent(depth - 1)}  - ${node.name}: ${stringify(node.oldValue, depth)}`,
-    `${getIndent(depth - 1)}  + ${node.name}: ${stringify(node.newValue, depth)}`,
+    `${makeIndent(depth - 1)}  - ${node.name}: ${stringify(node.oldValue, depth)}`,
+    `${makeIndent(depth - 1)}  + ${node.name}: ${stringify(node.newValue, depth)}`,
   ],
-  parent: (node, depth, render) => `${getIndent(depth)}${node.name}: {\n${render(node.children, depth + 1)}\n${getIndent(depth)}}`,
+  parent: (node, depth, render) => `${makeIndent(depth)}${node.name}: {\n${render(node.children, depth + 1)}\n${makeIndent(depth)}}`,
 };
 
 const renderDiff = (tree, depth) => {
